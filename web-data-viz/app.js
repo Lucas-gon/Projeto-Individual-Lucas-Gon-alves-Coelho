b96b078af0877a1fd85af2ba53195db672a826fe
Lucas Coelho <lucas.coelho@sptech.school>
2026-07-13 22:39:50 -0300

    Feat: Adicionando o caminho da nova rota(tentativas).

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -21,6 +21,8 @@ var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
+var tentativasRouter = require("./src/routes/tentativas");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
+app.use("/tentativas", tentativasRouter);
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
